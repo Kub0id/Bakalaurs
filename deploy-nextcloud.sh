b274bd88adb2766fcd22ecf407c2a402c1339530
Krists <48671066+Kub0id@users.noreply.github.com>
2023-04-24 22:41:41 +0300

    Update deploy-nextcloud.sh

--- deploy-nextcloud.sh
+++ deploy-nextcloud.sh
@@ -12,15 +12,16 @@ echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docke
 apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 # Edit Config
+echo "Ievadi VPN lietotāju skaitu no 1 līdz 244"
 read USERS
 if ! [[ "$USERS" =~ ^[0-9]+$ ]] ;
  then exec >&2; echo "Kļūda, nav cipars"; exit 1
 fi
-sed -i "s/PEERS=1.*/PEERS=$USERS/g" ./Bakalaurs/docker-compose.yml
+sed -i "s/PEERS=1.*/PEERS=$USERS/g" docker-compose.yml
 PASS=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 12 ; echo '')
-sed -i "s/MYSQL_PASSWORD=.*/MYSQL_PASSWORD=$PASS/g" ./Bakalaurs/docker-compose.yml
+sed -i "s/MYSQL_PASSWORD=.*/MYSQL_PASSWORD=$PASS/g" docker-compose.yml
 PASS=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 12 ; echo '')
-sed -i "s/MYSQL_ROOT_PASSWORD=.*/MYSQL_ROOT_PASSWORD=$PASS/g" ./Bakalaurs/docker-compose.yml
+sed -i "s/MYSQL_ROOT_PASSWORD=.*/MYSQL_ROOT_PASSWORD=$PASS/g" docker-compose.yml
 
 # Make containers
 docker compose up -d
